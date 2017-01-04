07caa6c3cb96c5eabd5ca2c5e7cea9fbec2e9f77
Jefedeltejas <blixt@localhost.localdomain>
2017-01-04 19:41:50 +0200

    squares a number - squareNum src code

---
+++ squareNum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/* main program  */
+int main(void)
+{
+    /*  these are my variables  */
+    int number, output;
+
+    /* prompt the user for data  */
+    printf("Enter a number: ");
+
+    /* scan the user input from console*/
+    scanf("%d", &number);
+
+    /* make the calculation*/
+    output = number * number;
+
+    /*print the result on screen */ 
+    printf("The square of %d is %d\n", number, output);
+    return 0;
+
+
+
+}
