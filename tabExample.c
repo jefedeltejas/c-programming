ea4d9f648c6c4b53151f799d21c7e1c607c79c7e
Jefedeltejas <blixt@localhost.localdomain>
2017-01-05 02:52:37 +0200

    changed text - tabExample

--- tabExample.c
+++ tabExample.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    printf("Some stuff\tSome more stuff\nThird pile of stuff\tFourth chunck \n");
+    printf("Some stuff\tSome more stuff\nThird pile of stuff\tFourth chunck of stuff\n");
 return 0;
 }
