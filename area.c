8413bd37171967d24742abc0bb95ed5946634ffc
Jefedeltejas <blixt@localhost.localdomain>
2017-01-07 19:55:07 +0200

    1st commit - still working, skeched alg, started code

---
+++ area.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int radius, area;
+
+int main(void) 
+{
+    /* prompt the user for radius  */
+    printf( "Enter radius (i.e. 10): ");
+    
+    
+    /* take the input from the console   */
+    scanf("%d", &radius);
+    
+    /* calculate area with radius given by usr input  */ 
+
+    /* print the result in the console  */
+
+    /* close the program  */
+    return 0;
+    /*   */ 
+
+}
