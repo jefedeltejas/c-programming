3716daf5e05a5908fb7a00c6278515277771dbc7
Jefedeltejas <blixt@localhost.localdomain>
2017-01-15 07:05:09 +0200

    finished out prog - now testing

--- euro-converter.c
+++ euro-converter.c
@@ -1,9 +1,18 @@
 #include <stdio.h> 
 
-float euro, markka, conversion-rate;
+float euro, markka, conversion-rate = 0.00, 0.00, 5.94573;
 
 int main(void) 
 {
+	// prompt usr for data 
 	printf("Enter an amout in FIM:");
 	scanf("%f", &markka);
+	
+	//calculate amount
+	euro = markka / conversion-rate;
+
+	//output
+	printf("FIM converted to euro: "%.2f", euro);
+
+	return 0;
 }
