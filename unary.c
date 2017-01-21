2823934e8a052ea833311b1a28b88b02947da781
Jefedeltejas <blixt@localhost.localdomain>
2017-01-21 17:35:35 +0200

    1st commit incrementor example

---
+++ unary.c
@@ -0,0 +1,38 @@
+/* Demonstrates unary operator prefix and postfix modes   */
+
+#include <stdio.h>
+
+int a, b;
+
+int main( void )
+{
+    // Sets a and b to 0 to start
+     a = b = 0;
+
+    // Start with the incremental operator
+    // Print them, decrementing each time.
+    // Use prefix mode for b, postfix mode for a
+
+    printf("Count up!\n");
+    printf("Post    Pre\n");
+    printf("%d    %d\n", a++, ++b);
+    printf("%d    %d\n", a++, ++b);
+    printf("%d    %d\n", a++, ++b);
+    printf("%d    %d\n", a++, ++b);
+    printf("%d    %d\n", a++, ++b);
+
+    printf("\nCurrent values of a and b:\n");
+    printf("%d    %d\n", a++, ++b);
+
+    printf("Count down!\n");
+    printf("\nPost  Pre");
+    print("\n%d    %d", a--, --b);
+    print("\n%d    %d", a--, --b);
+    print("\n%d    %d", a--, --b);
+    print("\n%d    %d", a--, --b);
+    print("\n%d    %d", a--, --b);
+    print("\n%d    %d\n", a--, --b);
+
+    return 0;
+
+}
