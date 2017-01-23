1c71aa417b30416ad00fa7be91acccc7c4e16538
Jefedeltejas <blixt@localhost.localdomain>
2017-01-24 00:15:33 +0200

    added two more results to output

--- sum2ints.c
+++ sum2ints.c
@@ -7,16 +7,18 @@ int main(void)
     // set the variables
     int integer1, integer2, sum, diff, prod;
 
-    printf("Enter the first number:\n");
+    printf("Enter the first number:");
     scanf("%i", &integer1);
-    printf("Enter the second number:\n");
+    printf("\nEnter the second number:");
     scanf("%i", &integer2);
 
     sum = integer1 + integer2;
     diff = integer1 - integer2;
     prod = integer1 * integer2;
     //show the output
-    printf ("%i+%i=%i\n", integer1, integer2, sum);
+    printf("%i+%i=%i\n", integer1, integer2, sum);
+    printf("%i-%i=%i\n", integer1, integer2, diff);
+    printf("%i*%i=%i\n", integer1, integer2, prod);
 
     return 0;
 }
