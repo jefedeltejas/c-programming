a16c8e1ffd93502b2c62747b40eb2a978883eb54
Jefedeltejas <jeff.jackson@dashiell.com>
2017-02-19 06:33:25 +0200

    changed initial increment var

--- factorial_loop.c
+++ factorial_loop.c
@@ -7,10 +7,8 @@ int main(void)
   printf("Enter an integer: ");
   scanf("%i", &user_number);
 
-  for(i = 0; i <= user_number; i++)
-  {
-    factorial = factorial * i;
-  }
+  for(i = 1; i <= user_number; i++)
+    factorial *= i;
   
   printf("The factorial of %i is %i\n", user_number, factorial);
 
