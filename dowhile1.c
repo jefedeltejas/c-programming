d8c7e4ee337b3f3e97680505c3baf8767a2a048a
Jefedeltejas <jeff.jackson@dashiell.com>
2017-02-19 05:50:04 +0200

    do while looper - 1st commit

---
+++ dowhile1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main()
+{
+  int number_entered, square;
+  do
+  {
+    printf("Enter an integer (exit with a negative number): ");
+    scanf("%d", &number_entered);
+
+    square = number_entered * number_entered;
+
+    if(number_entered >= 0) {
+      printf("The square of the number you entered: %d\n", square);
+    }
+    else if(number_entered < 0) {
+      printf("Exit program...\n");
+    }
+  }
+  while(number_entered > 0);
+
+  return 0;
+}
