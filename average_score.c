757b0b74cf4f2d491a73fdfb1735c05a3781ca29
Jefedeltejas <jeff.jackson@dashiell.com>
2017-02-19 07:58:29 +0200

    change to do while structure

--- average_score.c
+++ average_score.c
@@ -10,14 +10,14 @@ int main(void)
 
   printf("The program calculates the average of scores you enter.\n");
   printf("End with a negative integer.\n");
-  while(score >= 0)
+  do
   {
     printf("Enter score (4-10):");
     scanf("%d", &score);
     
     running_total += score;
     i++;
-  }
+  }while(score > 0);
   printf("You enterd %i scores\n", i);
   /* calculate avg */
   average = running_total / i;
