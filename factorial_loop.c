bb306e621e9f3df23439ed1eac9bd877972a672e
Jefedeltejas <jeff.jackson@dashiell.com>
2017-02-19 06:30:34 +0200

    changed the factorial calculation

--- factorial_loop.c
+++ factorial_loop.c
@@ -4,12 +4,12 @@ int main(void)
 {
   int i, factorial, user_number;
 
-  printf("Enter an integer: \n");
+  printf("Enter an integer: ");
   scanf("%i", &user_number);
 
   for(i = 0; i <= user_number; i++)
   {
-    factorial *= i;
+    factorial = factorial * i;
   }
   
   printf("The factorial of %i is %i\n", user_number, factorial);
