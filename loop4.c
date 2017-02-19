3f5e0367d59c964b7c933245d2507661544ea5ab
Jefedeltejas <jeff.jackson@dashiell.com>
2017-02-19 05:20:21 +0200

    added missing ;

--- loop4.c
+++ loop4.c
@@ -5,7 +5,7 @@ int main()
   int i, check;
   for(i = 1; i < 22; i++)
   {
-    check = printf("The current value of i is %d", i)
+    check = printf("The current value of i is %d", i);
 
     if(check == EOF)
     {
