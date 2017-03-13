155f1cdb40fb49888906a6d62125bff438332df8
Jefedeltejas <jeff.jackson@dashiell.com>
2017-03-13 06:31:32 +0200

    started simple calculator

---
+++ simple_calc.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int sum(int, int);
+float diff(int, int);
+float product(int, int);
+
+int main()
+{
+    int choice, first_number, second_number;
+    do{
+
+        printf("1:sum of two numbers\n2:difference of two numbers\n3:product of two numbers\n");
+        printf("<0: terminate the program\n");
+        printf("Select calculation:");
+        scanf("%d", &choice);
+	printf("Enter the first number:");
+	scanf("%d", &first_number);
+	printf("Enter the second number:");
+	scanf("%d", &second_number);
+        if(choice < 0) {
+          printf("Terminating the program...");
+        }
+        else if(choice == 1) {
+          sum(first_number, second_number);
+        }
+        else if(choice == 2) {
+          diff(first_number, second_number);
+        }
+        else if(choice == 3) {
+          product(first_number, second_number);
+        }
+        else {
+          printf("You entered an invalid number.\n\n");
+        }
+      }while(choice > 0);
+
+      return 0;
+}
+
+int sum(num1, num2)
+{
+    int num1, num2, sum;
+
+    sum = num1 + num2;
+
+    return sum;
+}
