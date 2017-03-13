e3426f2f188bb8cca963e09a7004510c2e3443a6
Jefedeltejas <jeff.jackson@dashiell.com>
2017-03-13 05:52:02 +0200

    add cat dog funktion

---
+++ catdog_func.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void cat(void);
+void dog(void);
+void cow(void);
+
+int main()
+{
+    int choice;
+    do{
+	
+	printf("1:cat\n2:dog\n3:cow\n");
+	printf("End with a negative number.\n");
+	printf("Select animal:");
+	scanf("%d", &choice);
+	if(choice < 0) {
+	  printf("Terminating the program...");
+	}
+	else if(choice == 1) {
+	  cat();
+	}
+	else if(choice == 2) {
+	  dog();
+	}
+	else if(choice == 3) {
+	  cow();
+	}
+	else {
+	  printf("You entered an invalid number.\n\n");
+	}
+      }while(choice > 0);
+
+      return 0;
+}
+
+void cat(void)
+{
+    printf("The cat says meow!");
+}
+void dog(void)
+{
+    print("The dog says bow wow wow, yippy yo yippy yea!");
+}
+void cow(void)
+{
+    printf("The cow says moo moo, suckah!!");
+}
