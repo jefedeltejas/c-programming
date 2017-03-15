199d00e225b51e64e7f5b6e587c4faeb70e00059
Jefedeltejas <jeff.jackson@dashiell.com>
2017-03-15 07:28:17 +0200

    forgot a return 0 in main

--- min-max.c
+++ min-max.c
@@ -23,8 +23,9 @@ int main(void)
     
     min = minNum(num1, num2, num3);
     max = maxNum(num1, num2, num3);
-    printf("Among the numbers you entered,\nthe largest was %d and the smallest was %d.", max, min);
-
+    printf("Among the numbers you entered,\nthe largest was %d and the smallest was %d.\n", max, min);
+ 
+    return 0;
 }
 
 int minNum(int a, int b, int c)
