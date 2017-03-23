0b0728a476e846d21310b03c63062b0eb276fe01
Jefedeltejas <jeff.jackson@dashiell.com>
2017-03-23 06:12:19 +0200

    modified the input loop to a while structure

--- backwardLoop.c
+++ backwardLoop.c
@@ -4,12 +4,16 @@ int main(void)
 {
 
   int numbers[100];
-  int index;
+  int index = 0;
+  
+  printf("Program asks for up to 100 numbers to print backward order.");
+  printf("\nTo stop the loop, input the number 1137");
 
-  for(index=0; index<100; index++)
+  while( index<100 || index!=1137)
   {
     printf("Enter the %d. number: ", index+1);
     scanf("%d", &numbers[index]);
+    index++;
   }
 
   printf("The numbers in reverse order: ");
