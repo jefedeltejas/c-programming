50e5dc560052ab0fa6ec9a0d839fa04d13c9e194
Jefedeltejas <jeff.jackson@dashiell.com>
2017-03-29 04:02:41 +0300

    practicing escape sequences

--- escape.c
+++ escape.c
@@ -26,7 +26,39 @@ int main( void )
 	if (choice == 2)
 	    print_report();
     }
-  
   }
+  printf("You chose to quit!\n");
 
+  return 0;
+}
+
+int get_menu_choice( void )
+{
+  int selection = 0;
+
+  do
+  {
+    printf( "\n" );
+    printf( "\n1 - Beep Computer");
+    printf( "\n2 - Display Report");
+    printf( "\n3 - Quit");
+    printf( "\n" );
+
+    printf( "\nEnter a selection:" );
+
+    scanf( "%d", &selection );
+
+  }while ( selection < 1 || selection > 3);
+
+  return selection;
+}
+
+void print_report( void )
+{
+  printf( "\nSAMPLE REPORT");
+  printf( "\n\nSequence\tMeaning" );
+  printf( "\n=========\t=======" );
+  printf( "\n\\a\t\tbell (alert)" );
+  printf( "\n\\b\t\tbackspace" );
+  printf( "\n...\t\t...");
 }
